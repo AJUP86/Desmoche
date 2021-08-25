5db87f9986dd7921c7d974db491175e3914aa67b
Alejandro Urroz <alejandrourroz86@gmail.com>
2021-08-25 22:39:03 +0200

    added seudocode file

---
+++ seudocode.js
@@ -0,0 +1,13 @@
+//the players can sort its hand by ascending order
+
+// Make the deal button activate and deactivate on depends on who's turn is.
+
+//Give color red to '♥', '♦' cards.
+
+//allow to form arrays of trio or four cards of the same rank
+
+//allow to form arrays of straigths of the same suit of three card or higher
+
+// the player who drop a game needs to pay with one card of his hand
+
+//anounce the winner when the game div reach 10 cards
